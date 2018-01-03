0e50533998a8c6b26d92fb18476ffdfbd0306f6b
Alexander F Rødseth <rodseth@gmail.com>
2018-01-03 14:38:06 +0100

    Update examples

--- examples/glut/main.cpp
+++ examples/glut/main.cpp
@@ -4,25 +4,33 @@
 #include <GL/glut.h>
 #endif
 
-void displayMe(void)
-{
+void displayMe(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.5, 0.5, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
+
+    glVertex3f(0.0, 0.0, 0.0);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+
     glEnd();
     glFlush();
 }
 
-int main(int argc, char** argv)
-{
+void processKeys(unsigned char key, int x, int y) {
+    // Quit if Esc or q was pressed
+    if (key == 27 || key == 113) {
+        exit(0);
+    }
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(320, 200);
     glutInitWindowPosition(200, 200);
     glutCreateWindow("Hello, World!");
+    glutKeyboardFunc(processKeys);
     glutDisplayFunc(displayMe);
     glutMainLoop();
     return 0;
