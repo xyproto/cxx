eedf4ea9b2ade43d1032dc7d50de6deb7747ad9d
Alexander F Rødseth <rodseth@gmail.com>
2018-01-03 14:25:48 +0100

    Add another example

---
+++ examples/args/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    for (int i=1; i < argc; i++) {
+        std::cout << "arg " << i << ": " << argv[i] << std::endl;
+    }
+    if (argc <= 1) {
+        std::cout << "no arguments" << std::endl;
+    }
+}
