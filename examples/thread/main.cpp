be3f4caf5cc42ce5bcee0ba087e9e485d2aef9b1
Alexander F Rødseth <rodseth@gmail.com>
2018-01-04 20:02:54 +0100

    Add an example that uses threading

---
+++ examples/thread/main.cpp
@@ -0,0 +1,27 @@
+// From http://en.cppreference.com/w/cpp/thread/async
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <future>
+ 
+template <typename RandomIt>
+int parallel_sum(RandomIt beg, RandomIt end)
+{
+    auto len = end - beg;
+    if (len < 1000)
+        return std::accumulate(beg, end, 0);
+ 
+    RandomIt mid = beg + len/2;
+    auto handle = std::async(std::launch::async,
+                             parallel_sum<RandomIt>, mid, end);
+    int sum = parallel_sum(beg, mid);
+    return sum + handle.get();
+}
+ 
+int main()
+{
+    std::vector<int> v(10000, 1);
+    std::cout << "The sum is " << parallel_sum(v.begin(), v.end()) << '\n';
+}
