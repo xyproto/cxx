7f96164554bf32111315df853d1c3fbd7a93ca40
Alexander F Rødseth <rodseth@gmail.com>
2018-01-04 10:25:12 +0100

    Update Ubuntu docker

--- tests/ubuntu/run.sh
+++ tests/ubuntu/run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker build --no-cache -t sakemake_ubuntu_17_10 .
-docker run --rm -t sakemake_ubuntu_17_10
+docker build --no-cache -t sakemake_ubuntu_17_10 . && docker run --rm -t sakemake_ubuntu_17_10
