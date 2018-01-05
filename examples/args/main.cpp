ff1c74d7eeed96b0893b9641235277a5e757579e
Alexander F Rødseth <rodseth@gmail.com>
2018-01-05 12:09:37 +0100

    Minor change to the args example

--- examples/args/main.cpp
+++ examples/args/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; ++i) {
     std::cout << "arg " << i << ": " << argv[i] << std::endl;
   }
   if (argc <= 1) {
