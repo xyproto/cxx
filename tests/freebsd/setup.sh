5f27c306814db72606b8cd702ae32df87aae2401
Alexander F Rødseth <rodseth@gmail.com>
2018-01-06 21:41:27 +0100

    Improve cross-compilation on FreeBSD

--- tests/freebsd/setup.sh
+++ tests/freebsd/setup.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-sudo pkg install freeglut gcc7 git glew gmake gtk3 mingw32 pkgconf qt5 scons sdl20
+#
+# Remember to search for packages case insensitively, with "sudo pkg search -i asdf"
+#
+
+# Update package repo
+sudo pkg update -f
+
+# Upgrade packages
+sudo pkg upgrade -y
+
+# Install packages required for Sakemake and for building the examples
+sudo pkg install -y bash figlet freeglut gcc7 git glew gmake gtk3 mingw32-gcc pkgconf qt5 scons sdl2
+
+# Clone and install Sakemake
 git clone https://github.com/xyproto/sakemake
 sudo gmake -C sakemake install
