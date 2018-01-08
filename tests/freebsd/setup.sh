d4bb6fe0d9674c968249f7b1d373676a9bcbc80d
Alexander F Rødseth <rodseth@gmail.com>
2018-01-08 14:08:54 +0100

    Skip building 64-bit Windows executables when on FreeBSD, for now

--- tests/freebsd/setup.sh
+++ tests/freebsd/setup.sh
@@ -10,9 +10,23 @@ pkg update -f
 pkg upgrade -y
 
 # Install packages required for Sakemake and for building the examples
-pkg install -y bash docker figlet freeglut gcc7 git glew gmake gtk3 pkgconf qt5 scons sdl2
+pkg install -y bash figlet freeglut gcc7 git glew gmake gtk3 pkgconf qt5 scons sdl2
+
+# Setup mingw32-w64 (looks like it requires ports to be set up first)
+#git clone https://github.com/takumin/ports-mingw-w64.git
+#(cd ports-mingw-w64/devel/mingw64-gcc; make install clean)
+
+# Setup docker
+#pkg install -y ca_root_nss docker-freebsd
+#kldload zfs
+#dd if=/dev/zero of=/usr/local/dockerfs bs=1024K count=1000
+#zpool create -f zroot /usr/local/dockerfs
+#zfs create -o mountpoint=/usr/docker zroot/docker
+#sysrc -f /etc/rc.conf docker_enable="YES"
+#service docker start
 
 # Clone and install Sakemake
 rm -rf sakemake || true
 git clone https://github.com/xyproto/sakemake
+#(cd sakemake; git checkout freebsd)
 gmake -C sakemake install
