1888dec145cdee23df9aac80e4957c86a23d833f
Alexander F Rødseth <rodseth@gmail.com>
2018-01-15 01:12:07 +0100

    Format example

--- examples/sfml/main.cpp
+++ examples/sfml/main.cpp
@@ -4,17 +4,16 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(256, 256), "Hello, SFML!");
     sf::CircleShape shape(128.f);
     shape.setFillColor(sf::Color::Green);
-    
-    while (window.isOpen())
-    {
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ||
+            sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
             window.close();
         }
 
