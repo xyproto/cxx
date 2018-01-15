9f15061a61bb2605918548c77453060f1ea169f2
Alexander F Rødseth <rodseth@gmail.com>
2018-01-15 01:11:28 +0100

    Add an example for SFML

---
+++ examples/sfml/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(256, 256), "Hello, SFML!");
+    sf::CircleShape shape(128.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+            window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
