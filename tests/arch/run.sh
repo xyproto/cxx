56b973e1e7fef7b8113f39a407bb4eb396404ab6
Alexander F Rødseth <rodseth@gmail.com>
2018-01-16 15:43:31 +0100

    Update tests
    mingw-w64 was moved to AUR
    
    Needs more testing, WIP

--- tests/arch/run.sh
+++ tests/arch/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build --no-cache -t sakemake:arch . && docker run --rm sakemake:arch
+docker build --no-cache -t sakemake:arch . && docker run --rm --name sakemake_arch sakemake:arch
