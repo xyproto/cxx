556e40e187239961be4040a99f55b37cd94b079a
Alexander F Rødseth <rodseth@gmail.com>
2018-01-16 14:57:22 +0100

    Update the tests for Ubuntu 17.10

--- tests/ubuntu/run.sh
+++ tests/ubuntu/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build --no-cache -t sakemake:ubuntu_17_10 . && docker run --rm sakemake:ubuntu_17_10
+docker build --no-cache -t sakemake:ubuntu_17_10 . && docker run --rm --name sakemake_ubuntu sakemake:ubuntu_17_10
