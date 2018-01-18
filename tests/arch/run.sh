0709b7c1875684f24f64a2e5ea068204902d35b5
Alexander F Rødseth <rodseth@gmail.com>
2018-01-18 14:24:39 +0100

    Further work on installing mingw64-w64-gcc in an Arch Linux docker

--- tests/arch/run.sh
+++ tests/arch/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker build --no-cache -t sakemake:arch . && docker run --rm --name sakemake_arch sakemake:arch
+#docker build --no-cache -t sakemake:arch . && docker run --rm --name sakemake_arch sakemake:arch
+docker build -t sakemake:arch . && docker run --rm --name sakemake_arch sakemake:arch
