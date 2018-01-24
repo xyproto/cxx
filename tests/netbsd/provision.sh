ead8f9d8a399ce12b14151de7a151211e7be1043
Alexander F Rødseth <rodseth@gmail.com>
2018-01-24 10:34:28 +0100

    Fix for NetBSD

--- tests/netbsd/provision.sh
+++ tests/netbsd/provision.sh
@@ -2,9 +2,9 @@
 
 set -v
 
-echo 'ftp://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.1/All' | sudo tee /usr/pkg/etc/pkgin/repositories.conf
+#echo 'ftp://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.1/All' | sudo tee /usr/pkg/etc/pkgin/repositories.conf
 
-sudo pkgin update
+#sudo pkgin update
 
 ## Install packages required for Sakemake and for building the examples
 sudo pkgin -y install SDL2 SDL2_image SDL2_mixer SDL2_net SDL2_ttf bash docker freeglut gcc7 pkgin install git glew gmake gtk3+ pkgconf qt5 scons
