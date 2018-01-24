45db6b030a078dcfe05ac67c85e71e465a6b0ac8
Alexander F Rødseth <rodseth@gmail.com>
2018-01-24 11:21:37 +0100

    Update NetBSD test

--- tests/netbsd/provision.sh
+++ tests/netbsd/provision.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-set -v
+set -x
 
-#echo 'ftp://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.1/All' | sudo tee /usr/pkg/etc/pkgin/repositories.conf
+# Select a package mirror
+echo 'ftp://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.1/All' | sudo tee /usr/pkg/etc/pkgin/repositories.conf
 
-#sudo pkgin update
+# pkgin -y is not needed for the update command
+sudo pkgin update
 
 ## Install packages required for Sakemake and for building the examples
 sudo pkgin -y install SDL2 SDL2_image SDL2_mixer SDL2_net SDL2_ttf bash docker freeglut gcc7 pkgin install git glew gmake gtk3+ pkgconf qt5 scons
 
-# sudo pkgin -y install mozilla-rootcerts
-# sudo mozilla-rootcerts install
+# Clone or pull sakemake
+test -d sakemake && (cd sakemake; git -c http.sslVerify=false pull origin master) || git -c http.sslVerify=false clone 'https://github.com/xyproto/sakemake'
 
-# Setup mingw32-w64 (looks like it requires ports to be set up first)
-#git clone https://github.com/takumin/ports-mingw-w64.git
-#(cd ports-mingw-w64/devel/mingw64-gcc; make install clean)
-
-# Clone and install Sakemake
-rm -rf sakemake || true
-#git clone https://github.com/xyproto/sakemake
-git -c http.sslVerify=false clone https://github.com/xyproto/sakemake
+# Install Sakemake
 gmake -C sakemake install
