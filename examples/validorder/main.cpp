0510c5a509399f4eb92f320be263f7a32d6483be
Alexander F. Rødseth <rodseth@gmail.com>
2018-06-29 10:01:13 +0200

    Format the validorder example

--- examples/validorder/main.cpp
+++ examples/validorder/main.cpp
@@ -1,5 +1,5 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 using namespace std::string_literals;
 
@@ -8,7 +8,9 @@ int main()
     std::string s = "but I have heard it works even if you don't believe in it";
 
     // In C++17, this is no longer undefined behavior
-    std::cout << s.replace(0, 4, "").replace(s.find("even"), 4, "only").replace(s.find(" don't"), 6, "") << std::endl;
+    std::cout
+        << s.replace(0, 4, "").replace(s.find("even"), 4, "only").replace(s.find(" don't"), 6, "")
+        << std::endl;
 
     // The example is from https://stackoverflow.com/a/38501596/131264
 }
