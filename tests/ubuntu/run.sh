b090aa3a51444c7aaf2cff6d60da211427b6309d
Alexander F. Rødseth <rodseth@gmail.com>
2018-10-29 12:27:14 -0700

    Update the Ubuntu CI test

--- tests/ubuntu/run.sh
+++ tests/ubuntu/run.sh
@@ -2,4 +2,4 @@
 scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdir"
 
-docker build --no-cache -t sakemake:ubuntu_17_10 . && docker run --rm --name sakemake_ubuntu sakemake:ubuntu_17_10
+docker build --no-cache -t sakemake:ubuntu_18_10 . && docker run --rm --name sakemake_ubuntu sakemake:ubuntu_18_10
