9b369e9223b18369bf38d4bfd89d4c96b91fb4a1
Alexander F. Rødseth <rodseth@gmail.com>
2018-11-05 22:02:28 +0100

    Don't specify a provider for the FreeBSD vagrant configuration

--- tests/freebsd/run.sh
+++ tests/freebsd/run.sh
@@ -1,5 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdir"
 
-vagrant up --provision
+#vagrant-plugin-install() {
+#  local plugin="$1"
+#  msg2 "Installing the $plugin plugin, if not already installed"
+#  vagrant plugin list | grep -q "$plugin" || vagrant plugin install "$plugin"
+#}
+
+main() {
+#  msg2 'Listing Vagrant plugins'
+#  vagrant plugin list
+#
+#  vagrant-plugin-install pkg-config
+#  vagrant-plugin-install vagrant-libvirt
+#
+#  msg2 'Vagrant up'
+#  vagrant up --provision --provider=libvirt
+  vagrant up --provision
+}
+
+main "$@"
