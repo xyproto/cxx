b030423343c3d11bde77e54ec5276912cc7e075c
Alexander F. Rødseth <rodseth@gmail.com>
2018-11-15 14:43:44 +0100

    Update NetBSD test scripts

--- tests/netbsd/provision.sh
+++ tests/netbsd/provision.sh
@@ -12,7 +12,7 @@ echo 'ftp://ftp.fr.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.0/All' | sudo t
 sudo pkgin update
 
 # install basic tools
-sudo pkgin -y install bash git gmake pkgconf python3
+sudo pkgin -y install bash git gmake pkgconf python37
 
 # Install packages required for Sakemake and for building the examples
 sudo pkgin -y install SDL2 SDL2_image SDL2_mixer SDL2_net SDL2_ttf docker freeglut gcc7 glew glm glut openal qt5 scons boost
@@ -20,6 +20,9 @@ sudo pkgin -y install SDL2 SDL2_image SDL2_mixer SDL2_net SDL2_ttf docker freegl
 
 # Clone or pull sakemake
 test -d sakemake && (cd sakemake; git -c http.sslVerify=false pull origin master) || git -c http.sslVerify=false clone 'https://github.com/xyproto/sakemake'
+
+# Fix permissions
+chown -R vagrant:users .
 
 # Install Sakemake
 gmake -C sakemake install
