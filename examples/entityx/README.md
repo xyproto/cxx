97cb452360c315b3805b21a835c059ebb1e26b7b
Alexander F. Rødseth <rodseth@gmail.com>
2018-11-15 12:12:24 +0100

    Add the example from EntityX

---
+++ examples/entityx/README.md
@@ -0,0 +1,11 @@
+# EntityX
+
+This is the unmodified EntityX example from the [entityx project](https://github.com/alecthomas/entityx/tree/master/examples) on GitHub.
+
+# Building
+
+    sm
+
+# Building without warnings
+
+    sm sloppy
--- tests/freebsd/runtests.sh
+++ tests/freebsd/runtests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sakemake/tests/all.py fastclean:build config sfml vulkan vulkan_glfw win64crate fastcgi boost boost_thread mixer notify openal qt5 sfml_audio
+sakemake/tests/all.py fastclean:build config sfml vulkan vulkan_glfw win64crate fastcgi boost boost_thread mixer notify openal qt5 sfml_audio entityx
