880b4d6691616d31194b2b29a533813d21181a90
Alexander F. Rødseth <rodseth@gmail.com>
2018-12-06 19:03:34 +0100

    Add generic lambda example

---
+++ examples/lambda/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std::string_literals;
+
+int main(int argc, char** argv)
+{
+    // Using the template syntax for generic lambdas that were introduced in C++20
+    auto twice = []<typename T>(T x) {
+        return x + x;
+    };
+
+    // twice can double anything that can be doubled
+    std::cout << twice("SNU"s) << " " << twice(21) << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
