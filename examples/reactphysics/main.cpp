7753bd429c82522b44de93a3504afee55e3f3d86
Alexander F. Rødseth <rodseth@gmail.com>
2020-03-14 13:02:16 +0100

    Add an example for ReactPhysics3D

---
+++ examples/reactphysics/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "mathematics/mathematics.h"
+
+using namespace reactphysics3d;
+
+int main()
+{
+    auto v3 = Vector3(1, 2, 3);
+    std::cout << v3.length() << std::endl;
+}
