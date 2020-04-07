b7b57d747688dc8b97b841221583e9912569f2ad
Alexander F. Rødseth <rodseth@gmail.com>
2020-04-07 11:56:26 +0200

    Update documentation for the GL4 example

--- examples/gl4_spirv/README.md
+++
@@ -1,5 +0,0 @@
-This is a modified version of:
-
-https://github.com/hikiko/gl4
-
-Unknown license: https://github.com/hikiko/gl4/issues/1
--- examples/gl4_spirv/main.cpp
+++ examples/gl4_spirv/main.cpp
@@ -1,3 +1,6 @@
+// Based on https://github.com/hikiko/gl4
+// Unknown license: https://github.com/hikiko/gl4/issues/1
+
 #include <cassert>
 #include <cmath>
 #include <cstddef>
