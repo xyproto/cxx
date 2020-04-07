f431cb6a7f80fd5b00c76a459ba9382a8e682a69
Alexander F. Rødseth <rodseth@gmail.com>
2020-04-07 13:11:44 +0200

    Update documentation

--- examples/gl4_spirv/main.cpp
+++ examples/gl4_spirv/main.cpp
@@ -1,5 +1,5 @@
-// Based on https://github.com/hikiko/gl4
-// Unknown license: https://github.com/hikiko/gl4/issues/1
+// Based on https://github.com/hikiko/gl4 (public domain)
+// by Eleni Maria Stea <elene.mst@gmail.com>
 
 #include <cassert>
 #include <cmath>
