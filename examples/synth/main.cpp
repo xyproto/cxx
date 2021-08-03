ecb4f3f6e86769cb80d1d0faf1c09612abfe9995
Alexander F. Rødseth <rodseth@gmail.com>
2021-08-03 15:58:58 +0200

    Update the synth example

--- examples/synth/main.cpp
+++ examples/synth/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     SDL_Event event;
 
-    std::cout << "Press some letters and see what happens. Press q to quit." << std::endl;
+    std::cout << "Press some letters on the keyboard and you should hear audio. Press q to quit." << std::endl;
 
     while (true) {
 
