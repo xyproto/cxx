0968cb42294806b97524c9e4247282ab5ca8712a
Alexander F. Rødseth <rodseth@gmail.com>
2024-07-23 12:44:55 +0200

    Update the GTK-4 example

--- examples/gtk4/main.cpp
+++ examples/gtk4/main.cpp
@@ -4,8 +4,8 @@ static void print_hello(GtkWidget* widget, gpointer data) {
     g_print("Hello World\n");
 }
 
-static void activate(GtkApplication* app, gpointer user_data) {
-
+static void activate(GtkApplication* app, gpointer user_data)
+{
     GtkWidget* window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
@@ -19,14 +19,14 @@ static void activate(GtkApplication* app, gpointer user_data) {
 
     gtk_window_set_child(GTK_WINDOW(window), button);
 
-    gtk_widget_show(window);
+    gtk_widget_set_visible(window, TRUE);
 }
 
-int main(int argc, char** argv) {
-    GtkApplication* app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+int main(int argc, char** argv)
+{
+    GtkApplication* app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
-
     return status;
 }
